d641e10cc7554ea2d94580667fb400c98a9266ef
nitini <nitin.iyer.1103@gmail.com>
2017-07-30 23:58:03 -0700

    "Added stuff to Video Details"

--- app/src/main/java/com/example/nitini/bjj_video_organizer_v1/VideoDetails.java
+++ app/src/main/java/com/example/nitini/bjj_video_organizer_v1/VideoDetails.java
@@ -1,18 +1,44 @@
 package com.example.nitini.bjj_video_organizer_v1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class VideoDetails extends AppCompatActivity {
+
+    String videoTitleString;
+    String videoThumbnailString;
+    String videoLinkString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_details);
         Intent intent = getIntent();
-        String videoTitle = intent.getStringExtra("videoTitle");
-        Log.d("TAGGY", videoTitle);
+        videoTitleString = intent.getStringExtra("videoTitle");
+        videoThumbnailString = intent.getStringExtra("thumbnail");
+        videoLinkString = intent.getStringExtra("link");
+
+        TextView videoTitle = (TextView) findViewById(R.id.videoTitle);
+
+        videoTitle.setText(videoTitleString);
+
+        ImageView videoThumbnail = (ImageView) findViewById(R.id.videoThumbnail);
+
+        Glide.with(this)
+                .load(videoThumbnailString)
+                .into(videoThumbnail);
+
+    }
+
+    public void playVideoFromDetails(View v) {
+        this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(videoLinkString)));
     }
 }
